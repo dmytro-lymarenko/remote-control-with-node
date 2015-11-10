2d952d6b13b86919c1a1f0e532311cbdecd263fb
dmytro-lymarenko <dmytro.lymarenko@gmail.com>
2015-11-10 22:30:37 +0200

    add exec

--- server/run.js
+++ server/run.js
@@ -1,8 +1,17 @@
 var server = require('http').createServer();
 var io = require('socket.io')(server);
 
+var exec = require('exec');
+
 io.on('connection', function(socket) {
 	console.log('connect');
+	
+	socket.on('exec', function(command) {
+		exec(command, function(err, out, code) {
+			socket.emit('error', err);
+			socket.emit('result', out);
+		});
+	});
 	
 	socket.on('disconnect', function() {
 		console.log('disconnect');
